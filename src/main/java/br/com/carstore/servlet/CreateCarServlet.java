8c960c08456fc69ce9603cf3ec317e75b404757a
nicolle88 <nicollelimafeitosa@gmail.com>
2026-09-24 21:38:07 -0300

    qualquer coisa

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,12 +14,18 @@ import java.io.IOException;
 public class CreateCarServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String carName = req.getParameter("car-name");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("O nome do carro é: " + carName);
+        String carName = request.getParameter("car-name");
 
-        req.getRequestDispatcher("index.html").forward(req, resp);
+        CarDao raw = new CarDao();
+        System.out.println(carName);
+        Car car = new Car();
+        car.setName(carName);
+        raw.createCar(car);
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+
     }
 
 }
---
+++ src/main/webapp/index.html
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Car Store</title>
+</head>
+<body>
+<h2>Create Car</h2>
+
+<form action="/create-car" method="post">
+
+    <label>Car Name</label>
+    <input type="text" name="car-name" id="car-name">
+
+    <button type="submit">Register</button>
+
+</form>
+</body>
+</html>
